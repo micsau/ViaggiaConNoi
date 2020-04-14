dd7784bd43cf698fa99462912299c582813c9399
micsau <michelesaulle98@gmail.com>
2020-04-14 14:33:11 +0200

    Correzioni finali login logout signup

--- Login/login.php
+++ Login/login.php
@@ -9,19 +9,19 @@ if (!$password) {                                 // se non trova scritto user e
   exit("la password non è corretta, riprova!");
 }
 $connessione = new mysqli("remotemysql.com:3306", "vlIGVKqVUg", "R6OA2FGr12", "vlIGVKqVUg"); //connessione al db.
-$sql = "SELECT * FROM Users where username=$username and password=$password";
+$sql = "SELECT * FROM ViaggiaConNoi where Username='$username' AND Password='$password'";
 $result = mysqli_query($connessione, $sql);   //eseguo la query.
-if ($result || mysqli_num_rows($result) == 0) {
+if (mysqli_num_rows($result) > 0) {
   $row = mysqli_fetch_assoc($result);
   $_SESSION["Nome"] = $username;
-  $_SESSION["Role"] = $row["role"];
   ob_start();
   header('Location: \main\main.html');  //mi posiziono su dizionario una volta effettuato il login.
   ob_end_flush();
   exit();
 } else {
   session_destroy();
-  exit("Utente non trovato!");
+  echo("Username o Password errati, Riprova!");
+  exit('<br><br><a class ="btn btn-primary" href="../Login/LoginPage.html"> Ritorna Indietro </button>');
 }
 ?>
 <!--fine login -->
