bafb8f1a843eee60b9ddc1358035a8c71220fbda
micsau <michelesaulle98@gmail.com>
2020-05-05 16:34:34 +0200

    fixed navbar link mail

--- main/main.php
+++ main/main.php
@@ -1,5 +1,6 @@
 <html>    <!--ViaggiaConNoi-->
   <head>
+    <link rel="icon" type="image/png" href="../assets/logo/logo-ico.png"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -32,21 +33,17 @@
               </form>
             </div>
             <div class="col-sm-4 offset-md-1 py-4">
-              <h4 class="text-white">Come Contattarci</h4>
+              <h4 class="text-white">Contatti</h4>
               <ul class="list-unstyled">
-                <li><a href="#" class="text-white">Seguici su Twitter</a></li>
-                <li><a href="#" class="text-white">Metti mi piace su Facebook</a></li>
-                <li><a href="#" class="text-white">Contattaci</a></li>
-              </ul>
+                <li><a href="mailto:michelesaulle98@gmail.com" class="text-white">Tramite e-mail</a></li></ul>
             </div>
           </div>
         </div>
       </div>
       <div class="navbar navbar-dark bg-dark box-shadow">
         <div class="container d-flex justify-content-between">
-          <a href="#" class="navbar-brand d-flex align-items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-            <strong>ViaggiaConNoi</strong>
+          <a href="/" class="navbar-brand d-flex align-items-center">
+            <img src="../assets/logo/logo.png" width=100 height="100" alt="">
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
