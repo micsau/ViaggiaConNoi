cd5d59d4cf4e78dfd2a02207f2db843833ad06b8
micsau <michelesaulle98@gmail.com>
2020-05-09 20:52:21 +0200

    sistemato success

--- main/buy/confirm/index.php
+++ main/buy/confirm/index.php
@@ -14,6 +14,8 @@
   $notti = $dest['notti'];
   $baseurl = $jConfig["BASE_URL"];
 
+
+
   // ***STARTING STRIPE CONFIGURATION***
   \Stripe\Stripe::setApiKey($jConfig["STRIPE_API_KEY"]);
 
@@ -36,16 +38,83 @@
   $_SESSION['id_checkout_stripe'] = $session['id'];
 
 ?>
-<button id="confirm">Confirm</button>
-<script src="https://js.stripe.com/v3/"></script>
-<script>
-  var stripe=Stripe("<?php echo $jConfig["STRIPE_API_KEY_PUBLIC"]?>");
-  var confirmBtn=document.getElementById("confirm");
-
-  confirmBtn.addEventListener("click",function(){
-    // Redirects to Stripe checkout page
-    stripe.redirectToCheckout({
-      sessionId:"<?php echo $session["id"]?>"
-    });
-  });
-</script>
+<html>    <!--ViaggiaConNoi-->
+  <head>
+    <link rel="icon" type="image/png" href="../../../assets/logo/logo-ico.png"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Viaggia Con Noi: divertiti viaggiando!">
+    <meta name="author" content="Michele Saulle">
+    <link rel="stylesheet" href="../../../css/main.css">
+    <title>ViaggiaConNoi</title>
+  </head>
+  <body>
+    <header>
+      <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+              <h4 class="text-white">ViaggiaConNoi</h4>
+              <p class="text-muted">ViaggiaConNoi startup innovativa per prenotare e viaggiare comodamente ed il risparmio è garantito</p>
+              <form action="/profile/index.php">
+                <button type="submit" class="btn btn-success">Profilo</button>
+              </form>
+              <form action="/logout/logout.php">
+                <button type="submit" class="btn btn-primary">logout</button>
+              </form>
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+              <h4 class="text-white">Contatti</h4>
+              <ul class="list-unstyled">
+                <li><a href="mailto:michelesaulle98@gmail.com" class="text-white">Tramite e-mail</a></li></ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="navbar navbar-dark bg-dark box-shadow">
+        <div class="container d-flex justify-content-between">
+          <a href="/" class="navbar-brand d-flex align-items-center">
+            <img src="../../../assets/logo/logo.png" width=100 height="100" alt="">
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </div>
+    </header>
+    <main role="main">
+      <div class="container">
+        <div class="row pt-3">
+          <div class="col">
+            <h5 class="mb-3">Clicca su Confirm per procedere al pagamento, altrimenti clicca indietro.</h5>
+            <button id="confirm" class="btn btn-success">Confirm</button>
+            <a href="/main/main.php" class="btn btn-danger">Indietro</a>
+            <script src="https://js.stripe.com/v3/"></script>
+            <script>
+              var stripe=Stripe("<?php echo $jConfig["STRIPE_API_KEY_PUBLIC"]?>");
+              var confirmBtn=document.getElementById("confirm");
+              confirmBtn.addEventListener("click",function(){
+                // Redirects to Stripe checkout page
+                stripe.redirectToCheckout({
+                  sessionId:"<?php echo $session["id"]?>"
+                });
+              });
+            </script>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer class="text-muted" style="padding-top: 5vh;">
+      <div class="container">
+        <p class="float-right">
+          <a href="#">Torna in Cima</a>
+        </p>
+        <p>ViaggiaConNoi creata da Michele Saulle, per bug e altri problemi contattatelo a michelesaulle98@gmail.com</p>
+      </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body> 
+</html>      
--- main/buy/success/index.php
+++ main/buy/success/index.php
@@ -1,20 +1,89 @@
-<!-- TODO ALL -->
 <?php 
-    session_start();
-    $username = $_SESSION["Nome"];
-    $id_destinazione = $_SESSION['id_dest'];
-    $stripe = $_SESSION['id_checkout_stripe'];
-    $config = file_get_contents('../../../config.json');
-    $jConfig = json_decode($config, true);
-    $connessione = new mysqli($jConfig["DB_HOST"], $jConfig["DB_USER"], $jConfig["DB_PASSWORD"], $jConfig["DB_NAME"]);
-    $sql = "SELECT * FROM Users WHERE username = '$username'";
-    $result = $connessione->query($sql);
-    $user = $result->fetch_assoc();
-    $idutente = $user['id']; 
-    $sql = "UPDATE Destinazioni SET isBought = true, id_user_fk = '$idutente' WHERE id = '$id_destinazione'";
-    $result = $connessione->query($sql);
-    $sql = "SELECT * FROM Destinazioni WHERE id = '$id_destinazione'";
-    $result = $connessione->query($sql);
-    $destinazione = $result->fetch_assoc();
-    print_r($destinazione); 
+session_start();
+$username = $_SESSION["Nome"];
+$id_destinazione = $_SESSION['id_dest'];
+$stripe = $_SESSION['id_checkout_stripe'];
+$config = file_get_contents('../../../config.json');
+$jConfig = json_decode($config, true);
+$connessione = new mysqli($jConfig["DB_HOST"], $jConfig["DB_USER"], $jConfig["DB_PASSWORD"], $jConfig["DB_NAME"]);
+$sql = "SELECT * FROM Users WHERE username = '$username'";
+$result = $connessione->query($sql);
+$user = $result->fetch_assoc();
+$idutente = $user['id']; 
+$sql = "UPDATE Destinazioni SET isBought = true, id_user_fk = '$idutente' WHERE id = '$id_destinazione'";
+$result = $connessione->query($sql);
+$sql = "SELECT * FROM Destinazioni WHERE id = '$id_destinazione'";
+$result = $connessione->query($sql);
+$destinazione = $result->fetch_assoc();
 ?>
+<html>    <!--ViaggiaConNoi-->
+  <head>
+    <link rel="icon" type="image/png" href="../../../assets/logo/logo-ico.png"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Viaggia Con Noi: divertiti viaggiando!">
+    <meta name="author" content="Michele Saulle">
+    <link rel="stylesheet" href="../../../css/main.css">
+    <title>ViaggiaConNoi</title>
+  </head>
+  <body>
+    <header>
+      <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+              <h4 class="text-white">ViaggiaConNoi</h4>
+              <p class="text-muted">ViaggiaConNoi startup innovativa per prenotare e viaggiare comodamente ed il risparmio è garantito</p>
+              <form action="/profile/index.php">
+                <button type="submit" class="btn btn-success">Profilo</button>
+              </form>
+              <form action="/logout/logout.php">
+                <button type="submit" class="btn btn-primary">logout</button>
+              </form>
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+              <h4 class="text-white">Contatti</h4>
+              <ul class="list-unstyled">
+                <li><a href="mailto:michelesaulle98@gmail.com" class="text-white">Tramite e-mail</a></li></ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="navbar navbar-dark bg-dark box-shadow">
+        <div class="container d-flex justify-content-between">
+          <a href="/" class="navbar-brand d-flex align-items-center">
+            <img src="../../../assets/logo/logo.png" width=100 height="100" alt="">
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </div>
+    </header>
+    <main role="main">
+      <div class="container">
+        <div class="row pt-3">
+          <div class="col">
+          <div class="alert alert-success">
+          <h5>Acquisto effettuato con successo!!</h5>
+          </div>
+          <a href="/profile/orders" class="btn btn-outline-primary">Visualizza Ordini</a>
+          <a href="/main/main.php" class="btn btn-outline-danger">Torna alla Home</a>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer class="text-muted" style="padding-top: 5vh;">
+      <div class="container">
+        <p class="float-right">
+          <a href="#">Torna in Cima</a>
+        </p>
+        <p>ViaggiaConNoi creata da Michele Saulle, per bug e altri problemi contattatelo a michelesaulle98@gmail.com</p>
+      </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body> 
+</html>
