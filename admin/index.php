20abba2de3b058c4b8785332279f5e774eba012b
GioDots <dodedodo96@gmail.com>
2020-05-10 17:14:38 +0200

    handle image input for create destination

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    error_reporting(E_ALL ^ E_DEPRECATED);
     $config = file_get_contents('../config.json');
     $jConfig = json_decode($config, true);
     $connessione = new mysqli($jConfig['DB_HOST'], $jConfig['DB_USER'], $jConfig['DB_PASSWORD'], $jConfig['DB_NAME']);
@@ -9,13 +10,6 @@
     $user = $result->fetch_assoc();
     if($user['role'] == 0){
         header("Location: /");
-    }
-    if(sizeof($_POST) > 0){
-      $countfiles = count($_FILES['immagini']['name']);
-      for($i=0;$i<$countfiles;$i++){
-        $filename = $_FILES['immagini']['name'][$i];
-        move_uploaded_file($_FILES['immagini']['tmp_name'][$i],'../uploads/'.$username.'.png');
-      }
     }
 ?>
 <!DOCTYPE html>
@@ -75,16 +69,71 @@
         </div>
       </div>
     </header>
-    <form action="/admin/index.php" method="POST" enctype='multipart/form-data'>
-        <input type="text" name="citta" class="form-control" placeholder="citta">    
-        <input type="number" name="prezzo" class="form-control" placeholder="prezzo">    
-        <input type="number" name="notti" class="form-control" placeholder="notti">
-        <input type="number" name="latitudine" class="form-control" placeholder="latitudine"> 
-        <input type="number" name="longitudine" class="form-control" placeholder="longitudine">
-        <input type="file" name="immagini[]" class="form-control-files" placeholder="immagini" multiple>
-        <textarea name="descrizione" class="form-control" placeholder="descrizione"></textarea>
-        <button type="submit" class="btn btn-success">Aggiungi</button>
-    </form>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <?php
+            if(sizeof($_POST) > 0){
+              $urlsArray = array();
+              require_once('../utils/s3.php');
+              $S3 = new S3($jConfig["AWS_ACCESS_KEY"], $jConfig["AWS_SECRET_KEY"]);
+              $bucketname = "viaggiaconnoiimages";
+              $target_dir = "../uploads/";
+              $uploadOk = 1;
+              $countfiles = count($_FILES['immagini']['name']);
+              for($i=0;$i<$countfiles;$i++){
+                $target_file = $target_dir . basename($_FILES['immagini']["name"][$i]);
+                $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+                $uploadname = "{$_POST['citta']}$i.png";
+                if (file_exists($target_file)) {
+                  echo '<h1 class="pt-2">Errore:</h1>';
+                  echo $errorDivOpen;
+                  echo "Sorry, file already exists.";
+                  echo $backButton;
+                  exit($divClose);
+                  $uploadOk = 0;
+                }
+                if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                && $imageFileType != "gif" ) {
+                  echo '<h1 class="pt-2">Errore:</h1>';
+                  echo $errorDivOpen;
+                  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                  echo $backButton;
+                  exit($divClose);
+                  $uploadOk = 0;
+                }
+                move_uploaded_file($_FILES['immagini']["tmp_name"][$i], $target_file);
+                $result = $S3::putObject($S3::inputFile($target_file,false),$bucketname, $uploadname, $S3::ACL_PUBLIC_READ);
+                unlink($target_file);
+                if($result != 1){
+                  echo '<h1 class="pt-2">Errore:</h1>';
+                  echo $errorDivOpen;
+                  echo "errore durante il caricamento dell'immagine!";
+                  echo $backButton;
+                  exit($divClose);
+                }
+                $uploadURL = 'https://' . $bucketname . '.s3.amazonaws.com/'.$uploadname;
+                array_push($urlsArray, $uploadURL);
+              }
+              print_r($urlsArray);
+            }else{
+              echo '
+                <form action="/admin/index.php" method="POST" enctype="multipart/form-data">
+                  <input type="text" name="citta" class="form-control" placeholder="citta">    
+                  <input type="number" name="prezzo" class="form-control" placeholder="prezzo">    
+                  <input type="number" name="notti" class="form-control" placeholder="notti">
+                  <input type="number" name="latitudine" class="form-control" placeholder="latitudine"> 
+                  <input type="number" name="longitudine" class="form-control" placeholder="longitudine">
+                  <input type="file" name="immagini[]" class="form-control-files" placeholder="immagini" multiple>
+                  <textarea name="descrizione" class="form-control" placeholder="descrizione"></textarea>
+                  <button type="submit" class="btn btn-success">Aggiungi</button>
+                </form>
+              ';
+            }
+          ?>
+        </div>
+      </div>
+    </div>
     <script
       src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
       integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
